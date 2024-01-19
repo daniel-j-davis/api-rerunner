a94458fe4df20eaa9e00ef498af6580417592855
Daniel Davis <me@danieldavis.dev>
2024-01-19 12:28:36 +0000

    stupdi

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,46 @@
 import WebSocket from 'ws';
 import * as lineReader from "line-reader";
-try {
-
-  const ws = new WebSocket('ws://localhost:8765', {
-    handshakeTimeout: 500
-  });
 
 
+const ws = new WebSocket('ws://localhost:8765', {
+  handshakeTimeout: 500
+});
 
-  ws.on('error', function error(err) {
-    console.log("Error: " + err.code);
-    ws.close();
-  });
+const isValidJSONString = (str: string): boolean => {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
+};
 
-  ws.on('open', function open() {
-    console.log("CONNECTED");
-    ws.close();
-  });
+lineReader.eachLine("C:/Users/danie/Development/api-rerunner/api_file.json", async (line, _): Promise<void> => {
+  await eventHandler(line);
+});
 
-  const isValidJSONString = (str: string): boolean => {
-    try {
-      JSON.parse(str);
-    } catch (e) {
-      return false;
-    }
-    return true;
-  };
+const eventHandler = async (event: any) => {
+  try {
+    if (
+      isValidJSONString(event) ||
+      isValidJSONString(event.substring(0, event.length - 1))
+    ) {
+      let validLine = event;
 
-  lineReader.eachLine("C:/Users/danie/Development/api-rerunner/api_file.json", async (line, _): Promise<void> => {
-    await eventHandler(line);
-  });
-
-  const eventHandler = async (event: any) => {
-    try {
-      if (
-        isValidJSONString(event) ||
-        isValidJSONString(event.substring(0, event.length - 1))
-      ) {
-        let validLine = event;
-
-        if (!isValidJSONString(validLine)) {
-          validLine = event.substring(0, event.length - 1);
-        }
-
-        const actualEvent = JSON.parse(validLine);
-
-        ws.send(JSON.stringify(actualEvent));
-
-        return Promise.resolve();
+      if (!isValidJSONString(validLine)) {
+        validLine = event.substring(0, event.length - 1);
       }
-    } catch (err) {
-      console.log(err);
 
-      return Promise.reject();
+      const actualEvent = JSON.parse(validLine);
+
+      ws.send(JSON.stringify(actualEvent));
+
+      return Promise.resolve();
     }
-  };
+  } catch (err) {
+    console.log(err);
 
-} catch (err) {
-  console.log(err);
-}
+    return Promise.reject();
+  }
+};
+
